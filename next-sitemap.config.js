5fc13a6360a9bdf0e7fde592c97d620cbbb3bd22
halamlee <halam.lee.lami@gmail.com>
2023-05-22 17:15:23 +0900

    chore: eslint 설치 및 문제 해결

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://lami-next-blog.vercel.app/',
+  siteUrl: 'https://lami-next-blog.vercel.app/',
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
